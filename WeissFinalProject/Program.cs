3643313e674bfaeea4dc2b31b81160f0843be3e3
weiss <weiss@DESKTOP-QI8HOQI>
2022-08-01 13:15:47 -0500

    Add project files.

---
+++ WeissFinalProject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeissFinalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What recipe you would like to try: ");
+            Console.WriteLine("Chicken Alfredo, Pizza, Beef and Broccoli, Tacos");
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+
+        public class Ingredients //what all goes in the meal
+        {
+
+        }
+
+        public class TimeofMeal //supper, dinner, lunch, breakfast, snack
+        { 
+        
+        }
+
+        public class TypeofMeal //Entrae, appetizer, soup etc..
+        {
+
+        }
+
+        public class Instructions //also known as directions
+        {
+
+        }
+
+
+
+    }
+}
